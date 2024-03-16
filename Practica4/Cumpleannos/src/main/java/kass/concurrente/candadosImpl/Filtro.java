1cff521d4cc748bd072f595dc8b90c55be4c9133
MixtliVN <75192271+MixtliVN@users.noreply.github.com>
2024-03-16 23:13:23 +0000

    Add files via upload

--- Practica4/Cumpleannos/src/main/java/kass/concurrente/candadosImpl/Filtro.java
+++ Practica4/Cumpleannos/src/main/java/kass/concurrente/candadosImpl/Filtro.java
@@ -12,9 +12,10 @@ import kass.concurrente.candados.Semaphore;
  */
 public class Filtro implements Semaphore {
 
-    private int[] nivel; 
-    private int[] victima;
-    private int max;
+    private volatile int[] nivel; 
+    private volatile int[] victima;
+    private volatile int max;
+    private volatile int permits;
 
     /**
      * Constructor del Filtro
@@ -28,6 +29,7 @@ public class Filtro implements Semaphore {
 	this.nivel = new int[hilos];
 	this.victima = new int[hilos];
 	this.max = maxHilosConcurrentes;
+	this.permits = maxHilosConcurrentes;
 	for (int i = 0; i < hilos; i++){
 	    nivel[i] = 0;
 	}
@@ -36,7 +38,8 @@ public class Filtro implements Semaphore {
     @Override
     public int getPermitsOnCriticalSection() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPermitsOnCriticalSection'");
+        //throw new UnsupportedOperationException("Unimplemented method 'getPermitsOnCriticalSection'");
+	return this.permits;
     }
 
     @Override
@@ -48,17 +51,25 @@ public class Filtro implements Semaphore {
 	for(int i = 1; i < this.nivel.length; i++){
 	    nivel[id] = i;
 	    victima[i] = id;
+	    /*
 	    boolean conflicts_exist = true;
 	    while (conflicts_exist) {
 		conflicts_exist = false;
-		for (int k = 1; k < this.nivel.length; k++) {
-		    if (k != id && nivel[k] >= i && victima[i] == id) {
+		for (int k = 0; k < this.nivel.length; k++) {
+		    if (k != id && nivel[k] >= i && victima[i] == id && permits <= 0) {
 			conflicts_exist = true;
 			break;
 		    }
 		}
 	    }
+	    */
+	    for (int k = 0; k < nivel.length; k++) {
+                while ((k != id) && (nivel[k] >= i && victima[i] == id) && (permits <= 0)) {
+                    //spin wait
+                }
+            }
 	}
+	this.permits = permits - 1;
     }
 
     @Override
@@ -68,6 +79,7 @@ public class Filtro implements Semaphore {
         //int i = (int) Thread.currentThread().getId();
 	int i = Integer.parseInt(Thread.currentThread().getName());
         nivel[i] = 0;
+	this.permits++;
     }
     
 }
